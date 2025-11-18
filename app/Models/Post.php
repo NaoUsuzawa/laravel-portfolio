22586ba2d535af1d1ec856f8b2b142b2dd3c4229
Haruka-Kamogawa <214371250+Haruka-Kamogawa@users.noreply.github.com>
2025-11-18 07:26:47 +0000

    Apply automatic changes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
-     use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
