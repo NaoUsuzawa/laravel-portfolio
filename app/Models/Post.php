6faf2429174413efccee27cb575ea2f7a7121858
hiroshi-hara2626 <204888127+hiroshi-hara2626@users.noreply.github.com>
2025-12-02 08:47:00 +0000

    Apply automatic changes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
